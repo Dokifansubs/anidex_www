55efd22de273fc2a803f5a1ff8be7986af151f27
Michiel (PROMETHEUS) <hegemansmichiel@gmail.com>
2014-08-08 17:05:58 +0200

    Fixed server crash on request for invalid torrent ID

--- routes/router.js
+++ routes/router.js
@@ -43,6 +43,7 @@ module.exports = function(app) {
 
     app.get('/torrent', function(req, res) {
         database.getTorrent(req.query.id, function(err, data) {
+            if (!data) { return res.render('torrent', {title: 'AniDex', user: req.user }); }
             data.created = moment(data.created);
             data.size = pretty(data.size);
             res.render('torrent', {title: 'AniDex', torrent: data, user: req.user});
