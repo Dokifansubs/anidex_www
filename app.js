4a4fac68df9565c0491bbd884f8b54c143bf7ba2
Jonatan Nilsson <jonatan@nilsson.is>
2014-08-12 15:00:36 +0000

    How I would structure the code

--- app.js
+++ app.js
@@ -16,54 +16,56 @@ var Database = require('./database/database');
 var app = module.exports = express();
 var database = new Database();
 
-app.configure(function() {
-    app.set('port', 8008);
-    app.set('views', __dirname + '/views');
-    app.set('view engine', 'jade');
-    app.use(express.bodyParser());
-    app.use(express.cookieParser());
-    app.use(express.session(sessionConfig));
-    app.use(express.methodOverride());
-    app.use(passport.initialize());
-    app.use(passport.session());
-    app.use(flash());
-    app.use(require('stylus').middleware({ src: __dirname + '/public' }));
-    app.use(express.static(__dirname + '/public'));
-});
+app.set('port', 8008);
+app.set('views', __dirname + '/views');
+app.set('view engine', 'jade');
+app.use(express.bodyParser());
+app.use(express.cookieParser());
+app.use(express.session(sessionConfig));
+app.use(express.methodOverride());
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(flash());
+app.use(require('stylus').middleware({ src: __dirname + '/public' }));
+app.use(express.static(__dirname + '/public'));
 
-app.configure('development', function() {
+if (app.get('env') === 'development') {
     app.use(express.errorHandler());
+}
 
-    passport.use(new LocalStrategy(function(username, password, done) {
-        database.findUser(username, function(err, user) {
-            if (err) { return done(err) }
-            if (!user) {
-                return done(null, false, { message: 'Incorrect username or password.' });
-            }
-            if (!user.verified()) {
-                return done(null, false, { message: 'Account not verified. Please check your email.' });
-            }
-            if (!user.validPassword(password)) {
-                return done(null, false, { message: 'Incorrect username or password.' });
-            }
-            return done(null, user);
-        });
-    }));
 
-    passport.serializeUser(function(user, done) {
-        done(null, user.id);
-    });
+// Configure passport handling
+var passHandler = require('./lib/helpers/passport');
+passport.use(new LocalStrategy(passHandler.login));
+passport.serializeUser(passHandler.serialize);
+passport.deserializeUser(passHandler.deserialize)
 
-    passport.deserializeUser(function(id, done) {
-        database.findUserId(id, function(err, user) {
-            done(null, user);
-        });
-    })
-});
 
-require('./routes/router')(app);
+/*
+ * Routes handlers
+ */
 
-http.createServer(app).listen(app.get('port'), function() {
+var home = require('./lib/request-handlers/home')
+app.get('/', home.index);
+
+var torrent = require('./lib/request-handlers/torrent');
+app.get('/torrent', torrent.single);
+app.get('/upload', torrent.upload);
+
+var profile = require('./lib/request-handlers/profile');
+app.get('/register', profile.register);
+app.get('/login', profile.loginForm);
+app.get('/logout', profile.logout);
+
+app.post('/login', passport.authenticate('local', {
+    successRedirect: '/',
+    failureRedirect: '/login',
+    failureFlash: true })
+);
+
+
+// Start the server
+app.listen(app.get('port'), function() {
     console.log("Express server listening on port " + app.get('port'));
     console.log("UPDATE EMAIL SYSTEM IN FINAL VERSION".red);
 });
