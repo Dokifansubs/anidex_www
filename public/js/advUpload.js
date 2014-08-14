b842cc9d5e84759807a228e95387a0fa0f594a84
Michiel (PROMETHEUS) <hegemansmichiel@gmail.com>
2014-08-14 21:30:19 +0200

    Guards against incorrect file upload

--- public/js/advUpload.js
+++ public/js/advUpload.js
@@ -14,28 +14,35 @@ doc.ondragover = function() { this.classList.add('hover'); return false; };
 doc.ondragend = function() { this.classList.remove('hover'); return false; };
 doc.ondragleave = function() { this.classList.remove('hover'); return false };
 doc.ondrop = function(event) {
-    event.preventDefault && event.preventDefault();
-    this.classList.remove('hover');
-    var url = 'http://' + location.hostname + ':8008/upload';
     var files = event.dataTransfer.files;
-    var formData = new FormData();
-    console.log('Dropped files: ');
-    console.log(files[0]);
+    if (files[0].type === 'application/x-bittorrent') {
+        event.preventDefault && event.preventDefault();
+        this.classList.remove('hover');
+        var url = 'http://' + location.hostname + ':8008/upload';
+        var formData = new FormData();
+        console.log('Dropped files: ');
+        console.log(files[0]);
 
-    var xhr = new XMLHttpRequest();
-    xhr.onreadystatechange = function() {
-        if (xhr.readyState == 4) {
-            var torrent = JSON.parse(xhr.response);
-            document.getElementById('fname').innerHTML = torrent.name;
-            document.getElementById('fsize').innerHTML = torrent.length;
-            document.getElementById('finfo').innerHTML = torrent.infoHash;
+        var xhr = new XMLHttpRequest();
+        xhr.onreadystatechange = function() {
+            if (xhr.readyState == 4) {
+                var torrent = JSON.parse(xhr.response);
+                document.getElementById('fname').innerHTML = torrent.name;
+                document.getElementById('fsize').innerHTML = torrent.length;
+                document.getElementById('finfo').innerHTML = torrent.infoHash;
+            }
         }
+
+        formData.append("torrent", files[0]);
+        xhr.open('post', url, true);
+
+        xhr.send(formData);
+
+        return false;
+    } else {
+        document.getElementById('fname').innerHTML = "Not a torrent file";
+        document.getElementById('fsize').innerHTML = "";
+        document.getElementById('finfo').innerHTML = "";
+        return false;
     }
-
-    formData.append("torrent", files[0]);
-    xhr.open('post', url, true);
-
-    xhr.send(formData);
-
-    return false;
 };
