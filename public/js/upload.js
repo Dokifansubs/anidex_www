d272d0f2138916b08bd1af7514be59362ebd46d0
Michiel (SOVEREIGN) <hegemansmichiel@gmail.com>
2014-08-14 13:57:08 +0200

    Dropdown menu fix

--- public/js/upload.js
+++ public/js/upload.js
@@ -3,6 +3,7 @@ var dndSupported = function() {
 	return ('draggable' in div) || ('ondragstart' in div && 'ondrop' in div);
 };
 
+/*
 var doc = document.getElementById('dropzone');
 doc.ondragover = function() { this.classList.add('hover'); return false; };
 doc.ondragend = function() { this.classList.remove('hover'); return false; };
@@ -16,3 +17,8 @@ doc.ondrop = function(event) {
 
 	return false;
 };
+*/
+
+$(document).on('click', '.dropdown-menu.dropdown-menu-form', function(e) {
+    e.stopPropagation();
+});
