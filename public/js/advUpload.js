cf393ab07c1a0bf463026c2c8700b09ca8ff3beb
Michiel (SOVEREIGN) <hegemansmichiel@gmail.com>
2014-08-15 20:25:04 +0200

    Advanced Uploader test

--- public/js/advUpload.js
+++ public/js/advUpload.js
@@ -9,6 +9,62 @@ console.log(location.hostname);
 
 var reader = new FileReader();
 
+var createDiv = function() {
+    return document.createElement("div");
+}
+
+var addTorrentElement = function(element) {
+    var list = document.getElementById('torrent-list');
+    list.insertBefore(element, list.firstChild);
+}
+
+var createTorrentElement = function(torrent) {
+    var container = createDiv();
+    container.setAttribute('class', 'torrent-container');
+    container.id = torrent.infoHash;
+
+    var category = createDiv();
+    category.setAttribute('class', 'torrent-category');
+
+    var deleteButton = createDiv();
+    deleteButton.setAttribute('class', 'torrent-delete');
+
+    var nameField = createDiv();
+    nameField.setAttribute('class', 'torrent-name');
+    nameField.innerHTML = torrent.name;
+
+    var sizeField = createDiv();
+    sizeField.setAttribute('class', 'torrent-size');
+    sizeField.innerHTML = torrent.size;
+
+    var langField = createDiv();
+    langField.setAttribute('class', 'torrent-lang');
+    langField.innerHTML = 'English';
+
+    var hentaiField = createDiv();
+    hentaiField.setAttribute('class', 'torrent-hentai');
+
+
+    var filesButton = createDiv();
+    filesButton.setAttribute('class', 'torrent-files');
+
+
+    var infoField = createDiv();
+    infoField.setAttribute('class', 'torrent-infoField');
+
+
+    sizeField.appendChild(filesButton);
+    sizeField.appendChild(hentaiField);
+    sizeField.appendChild(langField);
+    container.appendChild(category);
+    container.appendChild(deleteButton);
+    container.appendChild(nameField);
+    container.appendChild(sizeField);
+    //container.appendChild(infoField);
+
+    return container;
+}
+
 var doc = document.getElementById('dropzone');
 doc.ondragover = function() { this.classList.add('hover'); return false; };
 doc.ondragend = function() { this.classList.remove('hover'); return false; };
@@ -26,23 +82,23 @@ doc.ondrop = function(event) {
         var xhr = new XMLHttpRequest();
         xhr.onreadystatechange = function() {
             if (xhr.readyState == 4) {
-                var torrent = JSON.parse(xhr.response);
-                document.getElementById('fname').innerHTML = torrent.name;
-                document.getElementById('fsize').innerHTML = torrent.length;
-                document.getElementById('finfo').innerHTML = torrent.infoHash;
+                var torrents = JSON.parse(xhr.response);
+                for (torrent of torrents) {
+                    addTorrentElement(createTorrentElement(torrent));
+                }
             }
         }
 
-        formData.append("torrent", files[0]);
-        xhr.open('post', url, true);
+        for (file of files) {
+            formData.append('torrent', file);
+        }
 
+        xhr.open('post', url, true);
         xhr.send(formData);
 
         return false;
     } else {
-        document.getElementById('fname').innerHTML = "Not a torrent file";
-        document.getElementById('fsize').innerHTML = "";
-        document.getElementById('finfo').innerHTML = "";
+        alert('Not a torrent file, stop trying to break it!');
         return false;
     }
 };
