865b41850b6e050aa21d65dd1f4c460e40058dca
Michiel (SOVEREIGN) <hegemansmichiel@gmail.com>
2014-09-16 14:17:39 +0200

    Revert "Added missing semicolons and what not."
    This reverts commit 5701d999d79e399c34eb6dd033b9e67c8fd8465a.
    
    Borked beyond repair

--- app.js
+++ app.js
@@ -5,7 +5,7 @@
 
 var express = require('express');
 var http = require('http');
-var sessionConfig = require('./conf/session.json');
+var ServerConfig = require('./conf/server.json');
 var passport = require('passport');
 var LocalStrategy = require('passport-local').Strategy;
 var colors = require('colors');
@@ -15,12 +15,12 @@ var database = require('./lib/repositories/database');
 
 var app = module.exports = express();
 
-app.set('port', 8008);
+app.set('port', ServerConfig.port || 80);
 app.set('views', __dirname + '/views');
 app.set('view engine', 'jade');
-app.use(express.bodyParser());
+app.use(express.bodyParser({uploadDir: __dirname + '/public/torrents'}));
 app.use(express.cookieParser());
-app.use(express.session(sessionConfig));
+app.use(express.session(ServerConfig.session));
 app.use(express.methodOverride());
 app.use(passport.initialize());
 app.use(passport.session());
@@ -32,43 +32,42 @@ if (app.get('env') === 'development') {
     app.use(express.errorHandler());
 }
 
-
 // Configure passport handling
 var passHandler = require('./lib/helpers/passport');
-passHandler.init(database);
 passport.use(new LocalStrategy(passHandler.login));
 passport.serializeUser(passHandler.serialize);
 passport.deserializeUser(passHandler.deserialize)
 
-
 /*
  * Routes handlers
  */
-
-var home = require('./lib/request-handlers/home')
-home.init(database);
-app.get('/', home.index);
-
 var torrents = require('./lib/request-handlers/torrents');
-torrents.init(database);
+app.get('/', torrents.search);
 app.get('/torrents/:id', torrents.single);
-app.get('/upload', torrents.upload);
-//app.get('/upload', passHandler.ensureAuthenticated, torrents.upload);
+//app.get('/upload', torrents.upload);
+app.get('/upload', passHandler.ensureAuthenticated, torrents.upload);
 app.post('/upload', torrents.uploadFile);
 app.post('/parse', torrents.parseFile);
+/*
 app.get('/advupload', passHandler.ensureAuthenticated ,torrents.advUpload);
+*/
+app.get('/download/:id', torrents.downloadFile);
 
 var groups = require('./lib/request-handlers/groups');
-groups.init(database);
 app.get('/groups', groups.list);
 app.get('/groups/:id', groups.detail);
 
 var profile = require('./lib/request-handlers/profile');
-profile.init(database);
 app.get('/register', profile.register);
 app.get('/login', profile.login);
 app.get('/logout', profile.logout);
 app.get('/verify-account', profile.verify);
+app.post('/update-password', passHandler.ensureAuthenticated, profile.updatePassword);
+
+var management = require('./lib/request-handlers/management');
+// TODO: Add authentication middleware.
+app.get('/account', passHandler.ensureAuthenticated, management.user);
+app.get('/account-group/:id', passHandler.ensureAuthenticated, management.group);
 
 app.post('/login', passport.authenticate('local', {
     successRedirect: '/',
@@ -82,5 +81,4 @@ app.post('/register', profile.registerForm);
 // Start the server
 app.listen(app.get('port'), function() {
     console.log("Express server listening on port " + app.get('port'));
-    console.log("UPDATE EMAIL SYSTEM IN FINAL VERSION".red);
 });
